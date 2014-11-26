e9483bfaa58ba64cd17bf2af9c0b5ce661d76ef8
Pat <patrick.obrien91@gmail.com>
2014-11-26 09:40:11 -0600

    For those who don't want to map the json into dynamic classes, added "return_json" bool to each method and constructor to return the untouched json

--- RiotApiClient.py
+++ RiotApiClient.py
@@ -20,12 +20,13 @@ import urls
 
 
 class RiotApiClient:
-    def __init__(self, key, region):
+    def __init__(self, key, region, return_json: bool=False):
         self.region = region.lower()
         self.key = key
         self.client = Client.Client(urls.base.format(self.region))
         self.summoner_id = -1
         self.summoner = None
+        self.return_json = return_json
 
     def change_region(self, region):
         """
@@ -37,7 +38,7 @@ class RiotApiClient:
         self.summoner_id = -1
         self.client = Client.Client(urls.base.format(self.region))
 
-    def search(self, summoner_name):
+    def search(self, summoner_name, return_json: bool=False):
         r = Request.Request(urls.player_by_name)
         r.add_url_parameter('region', self.region)
         r.add_url_parameter('summonerNames', summoner_name)
@@ -45,9 +46,11 @@ class RiotApiClient:
 
         self.summoner = getattr(self.client.execute_with_return_struct(r), summoner_name)
         self.summoner_id = getattr(self.summoner, 'id', -1)
+        if return_json or self.return_json:
+            return self.client.execute(r).json()
         return self.summoner
 
-    def ranked_match_history(self):
+    def ranked_match_history(self, return_json: bool=False):
         if self.summoner is None:
             raise Exception("Must search for summoner first")
 
@@ -56,9 +59,11 @@ class RiotApiClient:
         r.add_url_parameter('summonerId', self.summoner_id)
         r.add_query_parameter('api_key', self.key)
 
+        if return_json or self.return_json:
+            return self.client.execute(r).json()
         return self.client.execute_with_return_struct(r)
 
-    def recent_match_history(self):
+    def recent_match_history(self, return_json: bool=False):
         if self.summoner is None:
             raise Exception("Must search for summoner first")
 
@@ -69,7 +74,7 @@ class RiotApiClient:
 
         return self.client.execute_with_return_struct(r)
 
-    def ranked_stats(self):
+    def ranked_stats(self, return_json: bool=False):
         if self.summoner is None:
             raise Exception("Must search for summoner first")
 
@@ -78,9 +83,11 @@ class RiotApiClient:
         r.add_url_parameter('summonerId', self.summoner_id)
         r.add_query_parameter('api_key', self.key)
 
+        if return_json or self.return_json:
+            return self.client.execute(r).json()
         return self.client.execute_with_return_struct(r)
 
-    def summary_stats(self):
+    def summary_stats(self, return_json: bool=False):
         if self.summoner is None:
             raise Exception("Must search for summoner first")
 
@@ -89,20 +96,26 @@ class RiotApiClient:
         r.add_url_parameter('summonerId', self.summoner_id)
         r.add_query_parameter('api_key', self.key)
 
+        if return_json or self.return_json:
+            return self.client.execute(r).json()
         return self.client.execute_with_return_struct(r)
 
-    def match_details(self, match_id, include_timeline: bool=True):
+    def match_details(self, match_id, include_timeline: bool=True, return_json: bool=False):
         r = Request.Request(urls.match_details)
         r.add_url_parameter('region', self.region)
         r.add_url_parameter('matchId', match_id)
         r.add_query_parameter('api_key', self.key)
         r.add_query_parameter('includeTimeline', include_timeline)
 
+        if return_json or self.return_json:
+            return self.client.execute(r).json()
         return self.client.execute_with_return_struct(r)
 
-    def champion_data(self):
+    def champion_data(self, return_json: bool=False):
         r = Request.Request(urls.champion_data)
         r.add_url_parameter('region', self.region)
         r.add_query_parameter('api_key', self.key)
 
+        if return_json or self.return_json:
+            return self.client.execute(r).json()
         return self.client.execute_with_return_struct(r)
