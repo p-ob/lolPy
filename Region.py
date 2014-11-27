a05ca387d38973651e6f4081d51b6fdf60c91cf9
Pat <patrick.obrien91@gmail.com>
2014-11-27 10:23:46 -0600

    Added Region ids that are valid for Riot's API

---
+++ Region.py
@@ -0,0 +1,30 @@
+__author__ = 'Patrick O\'Brien'
+''' COPYRIGHT 2014
+    This file is part of lolPy.
+
+    lolPy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    lolPy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with lolPy.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+class Region:
+    na = 'na'
+    euw = 'euw'
+    eune = 'eune'
+    br = 'br'
+    kr = 'kr'
+    lan = 'lan'
+    las = 'las'
+    oce = 'oce'
+    ru = 'ru'
+    tr = 'tr'
