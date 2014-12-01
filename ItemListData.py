1f82196e806490125988796de9d4bddb3869e8ee
Patrick O'Brien <patrick.obrien91@gmail.com>
2014-12-01 13:41:57 -0600

    Create ItemListData.py

---
+++ ItemListData.py
@@ -0,0 +1,40 @@
+__author__ = 'Patrick O\'Brien'
+''' COPYRIGHT 2014
+    This file is part of lolPy.
+
+    lolPy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    lolPy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with lolPy.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+class ItemListData:
+    All = 'all'
+    Colloq = 'colloq'
+    Consume_on_full = 'consumeOnFull'
+    Consumed = 'consumed'
+    Depth = 'depth'
+    From = 'from'
+    Gold = 'gold'
+    Groups = 'groups'
+    Hide_from_all = 'hideFromAll'
+    Image = 'image'
+    In_store = 'inStore'
+    Into = 'into'
+    Maps = 'maps'
+    Required_champion = 'requiredChampion'
+    Sanitized_description = 'sanitizedDescription'
+    Special_recipe = 'specialRecipe'
+    Stacks = 'stacks'
+    Stats = 'stats'
+    Tags = 'tags'
+    Tree = 'tree'
